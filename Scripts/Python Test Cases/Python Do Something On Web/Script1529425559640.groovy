13f07166a6d3bb6c49ef4af3d7dde4f595ed0100
Alex Smith <dev.alex.88@gmail.com>
2018-12-27 16:28:59 +0700

    refactor

--- Scripts/Python Test Cases/Python Do Something On Web/Script1529425559640.groovy
+++ Scripts/Python Test Cases/Python Do Something On Web/Script1529425559640.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('https://www.katalon.com')
+WebUI.openBrowser('')
 
 CustomKeywords.'demo.PythonKeywords.gotoGoogle'()
 
